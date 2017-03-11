a2c8514eb8d75b79bed801780d32e369ddef8f01
Kristopher Johnson <kris@kristopherjohnson.net>
2017-03-11 13:46:16 -0500

    Initial Commit

---
+++ kjchess/kjchess.h
@@ -0,0 +1,19 @@
+//
+//  kjchess.h
+//  kjchess
+//
+//  Created by Kristopher Johnson on 3/11/17.
+//  Copyright © 2017 Kristopher Johnson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for kjchess.
+FOUNDATION_EXPORT double kjchessVersionNumber;
+
+//! Project version string for kjchess.
+FOUNDATION_EXPORT const unsigned char kjchessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kjchess/PublicHeader.h>
+
+

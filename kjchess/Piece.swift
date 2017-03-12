09be99e1061d266debbc25396daa72c81597c217
Kristopher Johnson <kris@kristopherjohnson.net>
2017-03-12 08:38:34 -0400

    Merge PieceConstants.swift into Piece.swift

--- kjchess/Piece.swift
+++ kjchess/Piece.swift
@@ -47,3 +47,17 @@ extension Piece: Equatable {}
 public func ==(lhs: Piece, rhs: Piece) -> Bool {
     return lhs.player == rhs.player && lhs.kind == rhs.kind
 }
+
+public let WP = Piece(.white, .pawn)
+public let WN = Piece(.white, .knight)
+public let WB = Piece(.white, .bishop)
+public let WR = Piece(.white, .rook)
+public let WQ = Piece(.white, .queen)
+public let WK = Piece(.white, .king)
+
+public let BP = Piece(.black, .pawn)
+public let BN = Piece(.black, .knight)
+public let BB = Piece(.black, .bishop)
+public let BR = Piece(.black, .rook)
+public let BQ = Piece(.black, .queen)
+public let BK = Piece(.black, .king)

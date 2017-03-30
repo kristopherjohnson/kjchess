4abf744d45a118d1b872b53ed8f5895c33770fa8
Kristopher Johnson <kris@kristopherjohnson.net>
2017-03-30 13:35:40 -0400

    Continued work on UCI protocol

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ kjchess
 
 kjchess is a simple chess-playing engine, implemented in Swift.
 
+I've wanted to write a chess program since I was a teenager.  I'm finally doing it.  I don't expect this to be a strong player, and it won't be using any advanced chess implementation techniques.  I just want to have my own chess program.
+
+## Status
+
+kjchess is not usable yet.  The following features are implemented:
+
+- Can represent a position.
+- Can apply moves to a position.
+- Can determine legal moves for a position (except castling and en passant)
+
+Currently working on UCI protocol implementation so that kjchess can be used as an engine with a UCI-compatible GUI.
+
--- kjchess/UCIEngine.swift
+++ kjchess/UCIEngine.swift
@@ -23,6 +23,9 @@ public class UCIEngine {
     public var putLine: (String) -> ()
         = { print($0) }
 
+    /// If true, emit diagnostics.
+    public var debug: Bool = false
+
     /// Read UCI commands and process them until "quit" or end-of-stream.
     public func runCommandLoop() throws {
         while let line = getLine() {
@@ -57,13 +60,105 @@ public class UCIEngine {
             putLine("id author Kristopher Johnson")
             putLine("uciok")
 
+        case "debug":
+            onDebugCommand(tokens: tokens)
+
+        case "isready":
+            putLine("readyok")
+
+        case "setoption":
+            onSetOptionCommand(tokens: tokens)
+
+        case "register":
+            onRegisterCommand(tokens: tokens)
+
+        case "ucinewgame":
+            onNewGameCommand(tokens: tokens)
+
+        case "position":
+            onPositionCommand(tokens: tokens)
+
+        case "go":
+            onGoCommand(tokens: tokens)
+
+        case "stop":
+            onStopCommand(tokens: tokens)
+
+        case "ponderhit":
+            onPonderHitCommand(tokens: tokens)
+
         case "quit":
             return false
 
         default:
-            putLine("info string Unexpected command \(cmd)")
+            writeInfo("Unexpected command \(cmd)")
         }
 
         return true
+    }
+
+    private func onDebugCommand(tokens: [String]) {
+        if tokens.count > 1 {
+            switch tokens[1] {
+            case "on":
+                debug = true
+                writeInfo("debug on")
+            case "off":
+                debug = false
+            default:
+                if debug {
+                    writeInfo("Unrecognized argument: \(tokens)")
+                }
+            }
+        }
+        else if debug {
+            writeInfo("Missing argument to debug command")
+        }
+    }
+
+    private func onSetOptionCommand(tokens: [String]) {
+        if debug {
+            writeInfo("Ignoring command \(tokens)")
+        }
+    }
+
+    private func onRegisterCommand(tokens: [String]) {
+        if debug {
+            writeInfo("Ignoring command \(tokens)")
+        }
+    }
+
+    private func onNewGameCommand(tokens: [String]) {
+        if debug {
+            writeInfo("Ignoring command \(tokens)")
+        }
+    }
+
+    private func onPositionCommand(tokens: [String]) {
+        if debug {
+            writeInfo("Ignoring command \(tokens)")
+        }
+    }
+
+    private func onGoCommand(tokens: [String]) {
+        if debug {
+            writeInfo("Ignoring command \(tokens)")
+        }
+    }
+
+    private func onStopCommand(tokens: [String]) {
+        if debug {
+            writeInfo("Ignoring command \(tokens)")
+        }
+    }
+
+    private func onPonderHitCommand(tokens: [String]) {
+        if debug {
+            writeInfo("Ignoring command \(tokens)")
+        }
+    }
+
+    private func writeInfo(_ s: String) {
+        putLine("info string \(s)")
     }
 }
